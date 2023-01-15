6b4c50d9ab8d79aa97d5dfe868712075f98e2eb8
Ulugbek <ulugbekmuslitdinov@gmail.com>
2023-01-14 22:03:53 -0700

    HiLo game conditions added

--- java/HiLo/src/HiLo.java
+++ java/HiLo/src/HiLo.java
@@ -6,10 +6,18 @@ public class HiLo {
 		Scanner scan = new Scanner(System.in);
 		// Create the random number for user to guess
 		int theNumber = (int)(Math.random() * 100 + 1);
-		System.out.println( theNumber );
+//		System.out.println( theNumber );
 		int guess = 0;
-		System.out.println("Guess a number between 1 and 100: ");
-		guess = scan.nextInt();
-		System.out.println("You entered " + guess);
+		while (guess != theNumber) {
+			System.out.println("Guess a number between 1 and 100: ");
+			guess = scan.nextInt();
+			if (guess < theNumber) {
+				System.out.println(guess + " is too low. Try again.");
+			} else if (guess > theNumber) {
+				System.out.println(guess + " is too high. Try again.");
+			} else {
+				System.out.println(guess + " is correct. You win!");
+			}
+		}
 	}
 }

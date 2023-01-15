ba4056b6ba1f1b706dc480350af60c39d24abcfe
Ulugbek <ulugbekmuslitdinov@gmail.com>
2023-01-14 22:23:13 -0700

    play again feature

--- java/HiLo/src/HiLo.java
+++ java/HiLo/src/HiLo.java
@@ -4,20 +4,25 @@ public class HiLo {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		// Create the random number for user to guess
-		int theNumber = (int)(Math.random() * 100 + 1);
-//		System.out.println( theNumber );
-		int guess = 0;
-		while (guess != theNumber) {
-			System.out.println("Guess a number between 1 and 100: ");
-			guess = scan.nextInt();
-			if (guess < theNumber) {
-				System.out.println(guess + " is too low. Try again.");
-			} else if (guess > theNumber) {
-				System.out.println(guess + " is too high. Try again.");
-			} else {
-				System.out.println(guess + " is correct. You win!");
+		String playAgain = "";
+		do {
+			// Create the random number for user to guess
+			int theNumber = (int)(Math.random() * 100 + 1);
+			//		System.out.println( theNumber );
+			int guess = 0;
+			while (guess != theNumber) {
+				System.out.println("Guess a number between 1 and 100: ");
+				guess = scan.nextInt();
+				if (guess < theNumber) {
+					System.out.println(guess + " is too low. Try again.");
+				} else if (guess > theNumber) {
+					System.out.println(guess + " is too high. Try again.");
+				} else {
+					System.out.println(guess + " is correct. You win!");
+				}
 			}
-		}
+			System.out.println("Would you like to play again (y/n)?");
+			playAgain = scan.next();
+		} while (playAgain.equalsIgnoreCase("y"));
 	}
 }

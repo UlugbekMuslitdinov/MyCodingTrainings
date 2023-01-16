9c2962877d9fa057185e82b9cf558849e8941953
Ulugbek <ulugbekmuslitdinov@gmail.com>
2023-01-16 00:13:20 -0700

    Guessing game GUI

---
+++ java/GuessingGame/src/GuessingGame.java
@@ -0,0 +1,77 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GuessingGame extends JFrame {
+	private JTextField txtGuess;
+	private JLabel lblOutput;
+	private int theNumber;
+	
+	public void checkGuess() {
+		String guessText = txtGuess.getText();
+		String message = "";
+		int guess = Integer.parseInt(guessText);
+		if (guess < theNumber) {
+			message = guess + " is too low. Please Try again";
+		} else if (guess > theNumber) {
+			message = guess + " is too high. Please try again";
+		} else {
+			message = guess + " is correct. You win!";
+		}
+		lblOutput.setText(message);
+	}
+	
+	public void newGame() {
+		theNumber = (int)(Math.random() * 100 + 1);
+	}
+	
+	public GuessingGame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Ulugbek's HiLo Guessing Game");
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Ulugbek's HiLo Guessing Game");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 40, 416, 13);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Guess a number between 1 and 100");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_1.setBounds(71, 93, 193, 13);
+		getContentPane().add(lblNewLabel_1);
+		
+		txtGuess = new JTextField();
+		txtGuess.setBounds(274, 90, 60, 19);
+		getContentPane().add(txtGuess);
+		txtGuess.setColumns(10);
+		
+		JButton btnGuess = new JButton("Guess!");
+		btnGuess.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				checkGuess();
+			}
+		});
+		btnGuess.setBounds(175, 146, 85, 21);
+		getContentPane().add(btnGuess);
+		
+		lblOutput = new JLabel("Enter a number above and click Guess!");
+		lblOutput.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		lblOutput.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOutput.setBounds(10, 207, 416, 13);
+		getContentPane().add(lblOutput);
+	}
+
+	public static void main(String[] args) {
+		GuessingGame theGame = new GuessingGame();
+		theGame.newGame();
+		theGame.setSize(new Dimension(450,300));
+		theGame.setVisible(true);
+	}
+}

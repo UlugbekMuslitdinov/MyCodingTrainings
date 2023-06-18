38d6af63014554c9ebc3aab2bb8617acae11b653
Ulugbek <ulugbekmuslitdinov@gmail.com>
2023-06-18 13:23:23 +0500

    calculator finished

--- odin/calculator/index.js
+++ odin/calculator/index.js
@@ -1,6 +1,11 @@
 const nums = document.getElementsByClassName("num");
 const opers = document.getElementsByClassName("oper");
 const result = document.getElementsByClassName("res")
+const allButtons = document.getElementsByClassName("button")
+
+var numOne = ""
+var Operator = ""
+var numTwo = ""
 
 console.log(nums);
 console.log(opers);
@@ -13,10 +18,46 @@ console.log(result);
 //     result_window.innerHTML(self);
 // })));
 
-Array.prototype.forEach.call(nums, function(el) {
+Array.prototype.forEach.call(allButtons, function(el) {
     el.addEventListener("click", function(e) {
         const result_window = document.getElementById("result-window");
-        console.log(el);
-        result_window.innerText = el.innerHTML;
+        console.log(el.className);
+        if (el.className == "button num") {
+            if (Operator == "") {
+                numOne += el.innerHTML
+            } else {
+                numTwo += el.innerHTML
+            }
+        }
+        if (el.className == "button oper") {
+            if (numTwo != "") {
+                calculate(numOne, numTwo, Operator);
+            }
+            Operator = el.innerHTML;
+        }
+        if (el.className == "button res") {
+            calculate(numOne, numTwo, Operator);
+            Operator = "";
+        }
+        result_window.innerHTML = numOne + Operator + numTwo;
     });
 });
+
+function calculate(num1, num2, oper) {
+    var result;
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    if (oper == "+") {
+        result = num1 + num2;
+    } else if (oper == "-") {
+        result = num1 - num2;
+    } else if (oper == "*") {
+        result = num1 * num2;
+    } else if (oper == "/") {
+        result = num1 / num2;
+    }
+
+    numOne = String(result);
+    numTwo = "";
+}

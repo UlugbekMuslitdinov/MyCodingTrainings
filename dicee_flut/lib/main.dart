bfefeffa8fc54a910aa74bb7eaf9db065e8e9e96
Ulugbek <ulugbekmuslitdinov@gmail.com>
2023-07-19 18:31:36 +0500

    dice comlete

--- dicee_flut/lib/main.dart
+++ dicee_flut/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,51 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: MaterialButton(
+              onPressed: () {
+                setState(() {
+                  changeDiceFace();
+                });
+              },
+              splashColor: Colors.transparent,
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: MaterialButton(
+              onPressed: () {
+                setState(() {
+                  changeDiceFace();
+                });
+              },
+              splashColor: Colors.red,
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void changeDiceFace() {
+    rightDiceNumber = Random().nextInt(6) + 1;
+    leftDiceNumber = Random().nextInt(6) + 1;
   }
 }

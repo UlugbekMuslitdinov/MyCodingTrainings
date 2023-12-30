2e9a2c1150cf900057ac7aad301ff07fdc80edf7
Ulugbek Muslitdinov <61158544+UlugbekMuslitdinov@users.noreply.github.com>
2023-12-30 21:06:50 +0500

    strict mode removed

--- react/pizza-menu/src/index.js
+++ react/pizza-menu/src/index.js
@@ -6,8 +6,4 @@ function App() {
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);

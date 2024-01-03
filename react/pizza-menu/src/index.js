3932a485e777165139a8fa3b430cc89628d38a84
Ulugbek Muslitdinov <61158544+UlugbekMuslitdinov@users.noreply.github.com>
2024-01-03 22:14:41 +0500

    Nesting react components

--- react/pizza-menu/src/index.js
+++ react/pizza-menu/src/index.js
@@ -49,10 +49,29 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello react!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
   );
 }
 
